6cb4427d823e46f3fa07f3e4f39c599b663f3b9f
Maksim Shylau <shilaums@gmail.com>
2021-12-03 18:53:24 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,77 @@
 class JqueryImplement {
-    constructor() {
-      this.root = document.createElement("div");
-      document.body.append(this.root);
-    }
-  
-    
+  constructor() {
+    this.root = document.createElement("div");
+    document.body.append(this.root);
   }
-  
-  module.exports = JqueryImplement;
+
+  $(selector) {
+    const root = document.querySelector(selector);
+    this.root = root;
+    return this;
+  }
+
+  addClass(name) {
+    this.root.classList.add(name);
+    return this;
+  }
+
+  removeClass(name) {
+    this.root.classList.remove(name);
+    return this;
+  }
+
+  hasClass(name) {
+    if (this.root.classList.contains(name)) return true;
+  }
+
+  append(item) {
+    if (typeof item === "string") {
+      this.root.innerHTML += item;
+    } else {
+      this.root.append(item);
+    }
+    return this;
+  }
+
+  html(text) {
+    this.root.innerHTML = text;
+    return this;
+  }
+
+  attr(name, value = null) {
+    if (value) {
+      this.root.setAttribute(name, value);
+      return this;
+    }
+    return this.root.getAttribute(name);
+  }
+
+  empty() {
+    this.root.innerHTML = "";
+  }
+
+  children() {
+    this.root = this.root.childNodes;
+    return this;
+  }
+
+  css(style) {
+    this.root.style = style;
+    return this;
+  }
+
+  on(event, func) {
+    this.root.addEventListener(event, func);
+  }
+
+  one(event, func) {
+    this.root.addEventListener(event, func, { once: true });
+    return this;
+  }
+
+  each(func) {
+    this.root.forEach(func);
+  }
+}
+
+module.exports = JqueryImplement;
